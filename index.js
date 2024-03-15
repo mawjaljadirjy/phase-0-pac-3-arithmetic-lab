7cbf71353597ecf07c2e7ae636dfa49e3292c505
Mawj_Aljadirjy <eng_mawj_aljadirjy@yahoo.com>
2024-03-15 16:21:24 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+// index.js
+
+function add(a, b) {
+    return a + b;
+  }
+  
+function subtract(a, b) {
+    return a - b;
+  }
+  
+  
+function multiply(a, b) {
+    return a * b;
+  }  
+
+function divide(a, b) {
+    if (b !== 0) {
+      return a / b;
+    } else {
+      throw new Error("Division by zero is not allowed");
+    }
+}
+  
+function increment(n) {
+    return n + 1;
+  }
+function decrement(n) {
+    return n - 1;
+  }
+  
+
+function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  
+function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+  
+
+  
